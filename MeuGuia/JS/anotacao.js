d846b8a569299fbd11661662cb022e4b6651f9ec
Matheusmauriciom <150610041+Matheusmauriciom@users.noreply.github.com>
2024-04-02 11:14:50 -0300

    commit-anotacoesv1

---
+++ MeuGuia/JS/anotacao.js
@@ -0,0 +1,29 @@
+/* 
+Sintaxe básica: Domine os conceitos fundamentais, como variáveis, tipos de dados, operadores, estruturas de controle e funções.
+
+Manipulação do DOM: Aprenda a interagir com o DOM para manipular elementos HTML, estilos, eventos e criar interações dinâmicas nas páginas da web.
+
+Assincronia e AJAX: Compreenda como lidar com operações assíncronas, como fazer requisições HTTP assíncronas usando AJAX, para interagir com APIs e atualizar partes específicas de uma página.
+
+Manipulação de arrays e objetos: Domine a manipulação de arrays e objetos em JavaScript, utilizando métodos de array e técnicas de iteração para trabalhar com dados estruturados.
+
+Programação orientada a objetos: Familiarize-se com os conceitos básicos da programação orientada a objetos em JavaScript, como classes, herança, encapsulamento e polimorfismo.
+
+Manipulação de erros: Aprenda a utilizar blocos try-catch para capturar e tratar erros de forma adequada em seu código.
+
+Conhecimentos adicionais: Explore áreas complementares, como manipulação de strings, gerenciamento de exceções, manipulação de eventos e conhecimento de bibliotecas e frameworks populares, conforme necessário.*/
+
+// LET flexivel // CONST absoluta não pode ser alterada //
+// let dia = "" , dia="segunda", dia="terça", posso usar o dia varias vezes como string
+
+//Verificar o tipo de uma variavel typeof msg, typeof idade
+
+// {let funciona só o que está dentro dentro do bloco } função
+// const = (nunca vai muadar ) não importa onde
+
+// Booleano: isValid = true; false;
+
+// "use strict";
+// o JavaScript será interpretado de forma mais rigorosa
+
+// Shift + Alt + ↓ duplica linh
